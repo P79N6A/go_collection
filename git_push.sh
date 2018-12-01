c6c09a53654f53f672fb82b0de234249a0f04d2e
sheeks <740948792@qq.com>
2018-12-01 22:55:35 +0800

    modify git shell

--- git_push.sh
+++ git_push.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-git add .
-git commit -m "$1"
-git push origin master
+if [ $# == 0 ]
+then
+	echo "option error"
+elif [ $1 == "status" ]
+then
+	git status
+elif [ $1 == "push" ]
+then
+	git add .
+	git commit -m "$2"
+	git push origin master
+fi
