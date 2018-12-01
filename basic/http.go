26133abcd94150ac74d5d11519ba67a3ca1cda6d
sheeks <740948792@qq.com>
2018-12-01 22:40:37 +0800

    add index view model

--- basic/http.go
+++ basic/http.go
@@ -10,6 +10,11 @@ type User struct {
 	UserName string
 }
 
+type IndexViewModel struct {
+	Title string
+	User User
+}
+
 func printHelloWorld(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("hello world go web."))
 }
@@ -34,7 +39,14 @@ func TemplateFileTest(w http.ResponseWriter, r *http.Request) {
 	tpl.Execute(w,&user)
 }
 
+func TemplateIndexViewModelTest(w http.ResponseWriter, r *http.Request) {
+	user := User{UserName : "kevin"}
+	v := IndexViewModel{Title : "home page", User : user}
+	tpl,_ := template.ParseFiles("templates/index_view_model.html")
+	tpl.Execute(w,&v)
+}
+
 func main() {
-	http.HandleFunc("/",TemplateFileTest)
+	http.HandleFunc("/",TemplateIndexViewModelTest)
 	http.ListenAndServe(":8888",nil)
 }
